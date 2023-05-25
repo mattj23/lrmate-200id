a4594c6c233d8e07555eb3d3ce36271389f4de23
Bryan Rogers <bryan.rogers@astinc.us>
2023-05-25 16:46:22 -0400

    added basic functions to convert from raw to fanuc and inverse, updated example file

--- python/example.py
+++ python/example.py
@@ -1,6 +1,7 @@
 import open3d
 from lrmate.robot import Robot, Link, Transform, Vector, Joint
 from pathlib import Path
+from lrmate.joint_conversions import fanuc_to_raw, raw_to_fanuc
 
 
 def main():
@@ -12,13 +13,26 @@ def main():
     # Note that these are raw degrees from each joint's geometric zero position, and are not the same as the values
     # which would be put into the R-30iB controller to achieve the same pose. There is a direct conversion, but it
     # requires handling the J2/J3 joint interaction.
-    robot.set_joints_deg({"J1": 30, "J2": 30, "J3": 30, "J4": 30, "J5": 30, "J6": 30})
+
+    """for raw to fanuc"""
+    # raw_joints = {"J1": 13, "J2": 82, "J3": 12,"J4": 30, "J5": 17, "J6": 0}
+    # robot.set_joints_deg(raw_joints)
+    # fanuc_joints = raw_to_fanuc(raw_joints)
+    # print(f"Enter these joint values into RDK: {fanuc_joints}\n")
+
+    """for fanuc to raw"""
+    fanuc_joints = {"J1": 13, "J2": 82, "J3": 12,"J4": 30, "J5": 17, "J6": 0}
+    raw_joints = fanuc_to_raw(fanuc_joints)
+    robot.set_joints_deg(raw_joints)
+
 
     links = [link.mesh for link in robot.links.values()]
+    print(links)
     crosses = [link.cross for link in robot.links.values()]
     to_draw = links + crosses
 
-    end_transform = robot.end_link.transform
+    end_transform = robot.end_link.current_transform
+    print(end_transform.matrix)
     open3d.visualization.draw_geometries(to_draw)
 
 
---
+++ python/lrmate/joint_conversions.py
@@ -0,0 +1,32 @@
+import open3d
+from lrmate.robot import Robot, Link, Transform, Vector, Joint
+from pathlib import Path
+from typing import List, Optional, Dict, Union
+
+
+def main():
+    # fanuc = {"J1": 0, "J2": -90, "J3": 180, "J4": 0, "J5": 0, "J6": 0}
+    # raw = fanuc_to_raw(fanuc)
+
+    raw = {"J1": 0, "J2": 90, "J3": 0,"J4": 0, "J5": 0, "J6": 0}
+    fanuc = raw_to_fanuc(raw)
+
+
+    print(raw)
+    print(fanuc)
+
+
+def fanuc_to_raw(joint_bundle: Dict[str, float]) -> Dict:
+    raw_joints = {"J1": joint_bundle["J1"], "J2": -joint_bundle["J2"], "J3": joint_bundle["J2"] + joint_bundle["J3"],
+                  "J4": joint_bundle["J4"], "J5": joint_bundle["J5"], "J6": joint_bundle["J6"]}
+    return raw_joints
+
+
+def raw_to_fanuc(joint_bundle: Dict[str, float]) -> Dict:
+    fanuc_joints = {"J1": joint_bundle["J1"], "J2": -joint_bundle["J2"], "J3": joint_bundle["J2"] + joint_bundle["J3"],
+                    "J4": joint_bundle["J4"], "J5": joint_bundle["J5"], "J6": joint_bundle["J6"]}
+    return fanuc_joints
+
+
+if __name__ == '__main__':
+    main()
