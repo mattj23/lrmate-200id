78304a966f3e3b3aa31bfb385e48515e3aaf6275
Matt Jarvis <mattj23@gmail.com>
2023-05-26 17:20:59 -0400

    working pybotics

--- python/pbtest.py
+++ python/pbtest.py
@@ -1,7 +1,10 @@
 import numpy
 import math
+from pathlib import Path
 from pybotics.robot import Robot
 from lrmate.transforms import XyzWpr, Transform, Vector
+from lrmate.robot import RobotWithMeshes, LinkWithMesh, Transform, Vector, Joint
+import open3d
 
 
 def _from_deg(deg):
@@ -10,65 +13,27 @@ def _from_deg(deg):
 
 def main():
     expected = XyzWpr(465, 0, 365, -180, -90, 0)
+    root_path = Path.cwd().parent
+    robot = RobotWithMeshes(root_path.joinpath("urdf", "urdf", "LRMate-200iD.urdf"),
+                            mesh_path=root_path.joinpath("meshes", "med-res"),
+                            collision_mesh_path=root_path.joinpath("meshes", "convex"))
 
-    # params = numpy.array(
-    #     [
-    #         [_from_deg(90), 50, _from_deg(0), 0],
-    #         [_from_deg(0), 330, _from_deg(90), 0],
-    #         [_from_deg(90), 35, _from_deg(0), 0],
-    #         [_from_deg(-90), 0, _from_deg(0), 335],
-    #         [_from_deg(90), 0, _from_deg(0), 0],
-    #         [_from_deg(0), 0, _from_deg(0), 80],
-    #     ]
-    # )
-    #
+    to_draw = [link.mesh for link in robot.links.values()]
+    for mesh in to_draw:
+        mesh.scale(1000, center=[0, 0, 0])
+        mesh.translate([0, 0, -330])
 
-    # params = numpy.array(
-    #     [
-    #         [_from_deg(0), 0, _from_deg(90), 50, ],
-    #         [_from_deg(90), 0, _from_deg(0), 330, ],
-    #         [_from_deg(0), 0, _from_deg(90), 35, ],
-    #         [_from_deg(0), 335, _from_deg(-90), 0, ],
-    #         [_from_deg(0), 0, _from_deg(90), 0, ],
-    #         [_from_deg(0), 80, _from_deg(0), 0, ],
-    #     ]
-    # )
-    # params = numpy.array(
-    #     [
-    #         [_from_deg(90), 75, _from_deg(0), 330],
-    #         [_from_deg(0), 300, _from_deg(90), 0],
-    #         [_from_deg(90), 75, _from_deg(0), 0],
-    #         [_from_deg(-90), 0, _from_deg(0), 320],
-    #         [_from_deg(90), 0, _from_deg(0), 0],
-    #         [_from_deg(0), 0, _from_deg(0), 140],
-    #     ]
-    # )
-    #
-    # params = numpy.array([[_from_deg(90), 50, _from_deg(0), 0],
-    #                       [_from_deg(0), 330, _from_deg(90), 0],
-    #                       [_from_deg(90), 35, _from_deg(0), 0],
-    #                       [_from_deg(-90), 0, _from_deg(0), 335],
-    #                       [_from_deg(90), 0, _from_deg(0), 0],
-    #                       [_from_deg(0), 0, _from_deg(0), 80], ])
-    # joints = numpy.deg2rad([0, 0, 0, 0, 0, 0])
-    #
-    # n = 1
-    #
-    # robot = Robot.from_parameters(params[:n])
-    # joints = numpy.deg2rad(joints[:n])
-    # pose = robot.fk(joints)
-    # t = Transform(pose)
-    # print(t.matrix)
-    # # print(XyzWpr.from_transform(t))
-    # params = numpy.array([[_from_deg(90), 50, _from_deg(0), 0],
-    #                       [_from_deg(0), 330, _from_deg(90), 0],
-    #                       [_from_deg(90), 35, _from_deg(0), 0],
-    #                       [_from_deg(-90), 0, _from_deg(0), 335],
-    #                       [_from_deg(90), 0, _from_deg(0), 0],
-    #                       [_from_deg(0), 0, _from_deg(0), 80]][:len(joints)])
+    cross = open3d.geometry.TriangleMesh.create_coordinate_frame(size=300)
+    t = output(0, 0, 0, 0, 0, 30)
+    cross.transform(t)
+    to_draw.append(cross)
 
-    # output(0, 0, 0, 0, 0, 0)
-    output(0, 0)
+    # end_cross = open3d.geometry.TriangleMesh.create_coordinate_frame(size=300)
+    # end_cross.transform(t)
+    # to_draw.append(end_cross)
+
+    open3d.visualization.draw_geometries(to_draw)
+
 
 def output(*joints):
     params = numpy.array([[_from_deg(0), 0, _from_deg(0), 0],
@@ -84,6 +49,7 @@ def output(*joints):
     print(XyzWpr.from_transform(t))
     # print(Vector(1, 0, 0) * t)
     print(t.matrix.round(3))
+    return t.matrix
 
 
 if __name__ == '__main__':
